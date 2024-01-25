b580e24ad1a33c7469a9e5a64d5d54202aa2be08
Beethoven Souza <68762246+beethoven-souza@users.noreply.github.com>
2024-01-25 01:15:18 -0300

    Configuração da conexão com a base de dados

---
+++ RestAPI_ASPNET/Model/Context/SqlServerContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestAPI_ASPNET.Model.Context
+{
+    public class SqlServerContext : DbContext
+    {
+        public SqlServerContext() { }
+        public SqlServerContext(DbContextOptions<SqlServerContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Person> Persons { get; set; }
+
+    }
+}
